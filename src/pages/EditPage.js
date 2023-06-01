61c2f68353bbd653a8c1709f8d752615921575bd
Danah <danawattar@gmail.com>
2023-06-02 00:36:13 +0300

    updated validation for editPage , SignIn page

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import Sidebar from "../component/Sidebar";
-import {Link} from 'react-router-dom';
-import { useNavigate  } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 
 export default function EditPage() {
 
-  //Validate  
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [asset, setAsset] = useState({
+    Assets: '',
+    AssetID: '',
+    SerialNumber: '',
+    Model: '',
+    Brand: '',
+    Category: '',
+    Os: '',
+    Description: '',
+    Status: '',
+    date: ''
+  });
+
+  const handleChange = (event) => {
+    setAsset({ ...asset, [event.target.name]: event.target.value });
+  };
+
   const [validation, setValidation] = useState({
     Assets: '',
     AssetID: '',
@@ -15,11 +34,11 @@ export default function EditPage() {
     Os: '',
     Brand: ''
   });
-  
+
   const checkValidation = () => {
     let errors = { ...validation };
     let isValid = true;
-  
+
     // Assets name validation
     if (!asset.Assets.trim()) {
       errors.Assets = "Assets name is required";
@@ -30,7 +49,7 @@ export default function EditPage() {
     } else {
       errors.Assets = "";
     }
-  
+
     // Assets ID validation
     if (!asset.AssetID.trim()) {
       errors.AssetID = "Assets ID is required";
@@ -41,7 +60,7 @@ export default function EditPage() {
     } else {
       errors.AssetID = "";
     }
-  
+
     // Assets Model validation
     if (!asset.Model.trim()) {
       errors.Model = "Assets Model is required";
@@ -52,7 +71,7 @@ export default function EditPage() {
     } else {
       errors.Model = "";
     }
-  
+
     // Assets OS validation
     if (!asset.Os.trim()) {
       errors.Os = "Assets Os is required";
@@ -63,7 +82,7 @@ export default function EditPage() {
     } else {
       errors.Os = "";
     }
-  
+
     // Assets Brand validation
     if (!asset.Brand.trim()) {
       errors.Brand = "Assets Brand is required";
@@ -74,74 +93,42 @@ export default function EditPage() {
     } else {
       errors.Brand = "";
     }
-  
+
     setValidation(errors);
     return isValid;
   };
 
-  //Validate
+  useEffect(() => {
+    axios
+      .get(`/Asset/edit/${id}`)
+      .then((response) => {
+        setAsset(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => console.error(error));
 
+  }, [id]);
 
-    const navigate = useNavigate();
-    const [asset, setAsset] = useState({
-        Assets: '',
-        AssetID: '',
-        SerialNumber: '',
-        Model:'',
-        Brand:'',
-        Category:'',
-        Os:'',
-        Description:'',
-        Status:'',
-        date: ''
-      });
-    
-    const {Assets,AssetID,SerialNumber,Model,Brand,Category,Os,Description,Status,date}=asset;
-    const handleChange = (event) => {
-      setAsset({ ...asset, [event.target.name]: event.target.value });
-     
-    };
-
-    const { id } = useParams();
-    useEffect(() => {
-        axios
-            .get(`/Asset/edit/${id}`)
-            .then((response) => {
-                setAsset(response.data)
-                console.log(response.data)
-            })
-            .catch((error) => console.error(error));
-            
-    }, []); 
-
-    // validate-------------------------------
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const isValid = checkValidation();
-        if (isValid) {
-        axios
-
-          .put(`/Asset/edit/${id}`, asset) 
-          .then(()=>navigate("/Asset"))
-          .catch((error) => console.error(error));
-        }
-      };
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const isValid = checkValidation();
+    if (isValid) {
+      axios
+        .put(`/Asset/edit/${id}`, asset)
+        .then(() => navigate("/Asset"))
+        .catch((error) => console.error(error));
+    }
+  };
 
   return (
-    
-  <section class="bg-white ">
-            <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-                <h2 class="mb-4 text-xl font-bold text-gray-900">Edit Asset</h2>
-        
-<form onSubmit={handleSubmit} action="/Server"  className="mb-4 px-10">
-           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+    <section className="bg-white">
+      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h2 className="mb-4 text-xl font-bold text-gray-900">Edit Asset</h2>
+
+        <form onSubmit={handleSubmit} action="/Server" className="mb-4 px-10">
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
             <div className="w-full">
-              <label
-                className="block mb-2 text-sm font-medium text-gray-900"
-                htmlFor="Assets"
-              >
+              <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="Assets">
                 Asset:
               </label>
               <input
@@ -152,15 +139,11 @@ export default function EditPage() {
                 value={asset.Assets}
                 onChange={handleChange}
               />
-          {/* validate */}
+              {/* validate */}
               {validation.Assets && <p className="mt-2 text-sm text-red-600">{validation.Assets}</p>}
-
             </div>
             <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="AssetID"
-              >
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="AssetID">
                 ID:
               </label>
               <input
@@ -171,32 +154,32 @@ export default function EditPage() {
                 value={asset.AssetID}
                 onChange={handleChange}
               />
-              
+              {/* validate */}
+              {validation.AssetID && <p className="mt-2 text-sm text-red-600">{validation.AssetID}</p>}
             </div>
             <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="Status"
-              >
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="Status">
                 Status:
               </label>
-              <select    id="Status"  name="Status"  value={asset.Status}
-                onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
-                                <option selected="">Select Status</option>
-                                <option value="Available">Available</option>
-                                <option value="Disposed">Disposed</option>
-                                <option value="InUse">In Use</option>
-               </select>
+              <select
+                id="Status"
+                name="Status"
+                value={asset.Status}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+              >
+                <option>Select Status</option>
+                <option value="Available">Available</option>
+                <option value="Disposed">Disposed</option>
+                <option value="InUse">In Use</option>
+              </select>
             </div>
             <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="SerialNumber"
-              >
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="SerialNumber">
                 Serial Number:
               </label>
               <input
-               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 id="SerialNumber"
                 type="text"
                 name="SerialNumber"
@@ -205,10 +188,7 @@ export default function EditPage() {
               />
             </div>
             <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="Model"
-              >
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="Model">
                 Model:
               </label>
               <input
@@ -219,17 +199,15 @@ export default function EditPage() {
                 value={asset.Model}
                 onChange={handleChange}
               />
-              
+              {/* validate */}
+              {validation.Model && <p className="mt-2 text-sm text-red-600">{validation.Model}</p>}
             </div>
             <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="date"
-              >
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="date">
                 Date:
               </label>
               <input
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 id="date"
                 type="date"
                 name="date"
@@ -238,86 +216,87 @@ export default function EditPage() {
               />
             </div>
             <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="Brand"
-              >
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="Brand">
                 Brand:
               </label>
               <input
-             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 id="Brand"
                 type="text"
                 name="Brand"
                 value={asset.Brand}
                 onChange={handleChange}
               />
-              
+              {/* validate */}
+              {validation.Brand && <p className="mt-2 text-sm text-red-600">{validation.Brand}</p>}
             </div>
-
             <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="Category"
-              >
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="Category">
                 Category:
               </label>
-             
-               <select    id="Category"  name="Category"  value={asset.Category}
-                onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
-                                <option selected="">Select category</option>
-                                <option value="TV">TV/Monitors</option>
-                                <option value="PC">PC</option>
-                                <option value="GA">Gaming/Console</option>
-                                <option value="PH">Phones</option>
-               </select>
-            </div>
-            
-            <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="Os"
+              <select
+                id="Category"
+                name="Category"
+                value={asset.Category}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
               >
+                <option>Select category</option>
+                <option value="TV">TV/Monitors</option>
+                <option value="PC">PC</option>
+                <option value="GA">Gaming/Console</option>
+                <option value="PH">Phones</option>
+              </select>
+            </div>
+            <div className="w-full">
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="Os">
                 Os:
               </label>
               <input
-               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 id="Os"
                 type="text"
                 name="Os"
                 value={asset.Os}
                 onChange={handleChange}
               />
-              
+              {/* validate */}
+              {validation.Os && <p className="mt-2 text-sm text-red-600">{validation.Os}</p>}
             </div>
-            
             <div className="w-full">
-              <label
-                className="block text-gray-700 font-bold mb-2"
-                htmlFor="Description"
-              >
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="Description">
                 Description:
               </label>
-            
-               <textarea  id="Description"
+              <textarea
+                id="Description"
                 type="text"
                 name="Description"
                 value={asset.Description}
-                onChange={handleChange} rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Your description here"></textarea>
+                onChange={handleChange}
+                rows="8"
+                className="block p-2.5 border w-full h-full focus:ring-primary-500 focus:border-primary-500 text-gray-900 text-sm rounded-lg"
+              ></textarea>
             </div>
-            </div>
-            <div className="flex items-center justify-between">
-              <button
-                className="bg-teal-700 hover:bg-teal-900 text-white font-bold my-5 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-             
-                Edit Asset
-              </button>
-
-            </div>
-          </form> 
           </div>
-        </section>
-  )
+
+          <div className="mt-6">
+            <button
+              // type="submit"
+              // className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+              className="bg-teal-700 hover:bg-teal-900 text-white font-bold my-5 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Update Asset
+            </button>
+            <Link
+              to={`/Asset`}
+              className="ml-2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-gray-900 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+            >
+              Cancel
+            </Link>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
